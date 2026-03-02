03716b11bfc20150db2a23c0e39b34d1daca4a23
Ahana <sendatahana@gmail.com>
2026-03-02 11:37:28 +0530

    Refactor SQL query for customer and supplier comments

--- policies/p2.sql
+++ policies/p2.sql
@@ -2,7 +2,11 @@
 Customer and supplier who have the same comments
    --------------------------------------------------------- */
 SELECT s.*
-FROM customer c, supplier s
-     WHERE c.c_comment LIKE '%' || s.s_comment || '%'   
-AND c.c_acctbal > 0
-  AND s.s_acctbal > 0;
+FROM supplier s
+WHERE s.s_acctbal > 0
+  AND EXISTS (
+        SELECT 1
+        FROM customer c
+        WHERE c.c_acctbal > 0
+          AND c.c_comment LIKE '%' || s.s_comment || '%'
+  );
