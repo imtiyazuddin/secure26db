4b6508c10ece7f208cf5a07c9dd42a800d659a3a
Ahana <sendatahana@gmail.com>
2026-03-12 11:09:09 +0530

    Update p4_tiered.sql

--- policies/policies/p4_tiered.sql
+++ policies/policies/p4_tiered.sql
@@ -4,10 +4,11 @@
 
 SELECT ps.*
 FROM partsupp ps
-WHERE ps.ps_availqty > 100
+WHERE ps.ps_availqty > 9000
   AND EXISTS (
     SELECT 1
     FROM supplier s
     WHERE s.s_suppkey = ps.ps_suppkey
-      AND s.s_acctbal > 0
+      AND s.s_acctbal > 7999
   );
+
