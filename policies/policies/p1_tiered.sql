bd74f2bed95cca10cfdbcd7405429693e049a014
Ahana <sendatahana@gmail.com>
2026-03-12 11:43:03 +0530

    Update p1_tiered.sql

--- policies/policies/p1_tiered.sql
+++ policies/policies/p1_tiered.sql
@@ -4,15 +4,11 @@
 -- Complexity: SIMPLE (single EXISTS to dimension tables)
 
 SELECT c.*
-FROM customer c
+FROM customer c, nation n, region r
 WHERE c.c_acctbal > 7999
-  AND EXISTS (
-    SELECT 1
-    FROM nation n, region r
-    WHERE n.n_nationkey = c.c_nationkey
+  AND n.n_nationkey = c.c_nationkey
       AND n.n_regionkey = r.r_regionkey
-      AND r.r_name IN ('EUROPE', 'AMERICA')
-  );
+      AND r.r_name IN ('EUROPE', 'AMERICA');
 
 -- PREDICATE (for manual injection):
 -- c.c_acctbal > 0
@@ -23,4 +19,5 @@ WHERE c.c_acctbal > 7999
 --       AND n.n_regionkey = r.r_regionkey
 --       AND r.r_name IN ('EUROPE', 'AMERICA')
 --   )
+
 
