a49ba562f9ed8864207a64ad093a34ed66e8b9d5
Ahana <sendatahana@gmail.com>
2026-03-12 11:49:16 +0530

    Update p4_tiered.sql

--- policies/policies/p4_tiered.sql
+++ policies/policies/p4_tiered.sql
@@ -2,13 +2,21 @@
 -- References: supplier (Tier 1, has p2)
 -- Description: Partsupp from suppliers with positive balance and sufficient stock
 
-SELECT ps.*
+SELECT
+    ps.ps_partkey,
+    ps.ps_suppkey,
+    ps.ps_availqty,
+    ps.ps_supplycost,
+    ps.ps_comment
 FROM partsupp ps
+JOIN supplier s
+    ON s.s_suppkey = ps.ps_suppkey
 WHERE ps.ps_availqty > 9000
-  AND EXISTS (
-    SELECT 1
-    FROM supplier s
-    WHERE s.s_suppkey = ps.ps_suppkey
-      AND s.s_acctbal > 7999
-  );
+GROUP BY
+    ps.ps_partkey,
+    ps.ps_suppkey,
+    ps.ps_availqty,
+    ps.ps_supplycost,
+    ps.ps_comment
+HAVING MAX(CASE WHEN s.s_acctbal > 7999 THEN 1 ELSE 0 END) = 1;
 
